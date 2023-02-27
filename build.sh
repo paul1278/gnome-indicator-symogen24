49f9c551ec94c6c98af5690c2b0180241e23b84b
Paul <git@dev-paul.de>
2023-02-27 21:21:05 +0100

    Nice new build command

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+VERSION="$1"
+if [ "$VERSION" = "" ]; then
+  echo "No version."
+  exit 1
+fi
+echo Building version v$VERSION
 rm -r build 2>/dev/null
 mkdir build
+sed -i "s|Version: .*|Version: $1|g" DEBIAN/control
 cp -r {etc,DEBIAN,usr} build
-dpkg-deb --build --root-owner-group ./build nice.deb
+dpkg-deb --build --root-owner-group ./build gnome-indicator-symogen24-v$1.deb
 rm -r build
